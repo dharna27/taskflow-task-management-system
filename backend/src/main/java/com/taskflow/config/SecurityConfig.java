8b7501e6cb54638233f55b0be99fa43e803641f9
Dharna Gunidia <dgunidia@zoolarity.com>
2026-08-07 23:08:37 +0530

    feat: implement JWT authentication and secure task endpoints

--- backend/src/main/java/com/taskflow/config/SecurityConfig.java
+++ backend/src/main/java/com/taskflow/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.taskflow.config;
 
+import com.taskflow.service.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import com.taskflow.service.security.JwtAuthenticationFilter;
-import com.taskflow.service.security.CustomUserDetailsService;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -23,10 +29,20 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/users/register", "/api/users/login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
+
+        http.addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
 
         return http.build();
     }
--- backend/src/main/java/com/taskflow/service/jwt/JwtService.java
+++ backend/src/main/java/com/taskflow/service/jwt/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
 
 import java.util.Date;
 import io.jsonwebtoken.security.Keys;
@@ -14,6 +15,8 @@ public class JwtService {
 
     private static final String SECRET_KEY =
             "mySuperSecretKeyForTaskFlowApplicationJWTAuthentication1234567890";
+    private final SecretKey key =
+            Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
 
     // Generate JWT Token
     public String generateToken(String email) {
@@ -22,7 +25,7 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(key)
                 .compact();
     }
 
@@ -30,7 +33,7 @@ public class JwtService {
     public String extractEmail(String token) {
 
         Claims claims = Jwts.parser()
-                .verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                .verifyWith(key)
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
